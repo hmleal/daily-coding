3e34fcda1593393b8f7e8ce93c570d76a69d4fae
Henrique Leal <hm.leal@hotmail.com>
2021-09-14 15:06:19 +0100

    Templates for problem 01 and 02

---
+++ day01/solution.py
@@ -0,0 +1,6 @@
+def solution(k: int, numbers: list[int]) -> bool:
+    return True
+
+
+if __name__ == '__main__':
+    print(solution())
---
+++ day02/solution.py
@@ -0,0 +1,6 @@
+def solution(numbers: list[int]) -> list[int]:
+    return numbers
+
+
+if __name__ == '__main__':
+    print(solution)
