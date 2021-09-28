2de6f50ca4b5455427563f3af7e241ca23969bb7
Henrique Leal <hm.leal@hotmail.com>
2021-09-28 15:21:06 +0100

    feat: Add solutions for Problem 01 and 02

--- day01/solution.py
+++ day01/solution.py
@@ -1,6 +1,18 @@
 def solution(k: int, numbers: list[int]) -> bool:
-    return True
+    if k == 0:
+        return True
+
+    if k < 0:
+        return False
+
+    for n in numbers:
+        remainder = k - n
+
+        if solution(remainder, numbers):
+            return True
+
+    return False
 
 
-if __name__ == '__main__':
-    print(solution())
+if __name__ == "__main__":
+    print(solution(17, [10, 15, 3, 7]))
--- day02/solution.py
+++ day02/solution.py
@@ -1,6 +1,41 @@
-def solution(numbers: list[int]) -> list[int]:
-    return numbers
+def solution_one(numbers: list[int]) -> list[int]:
+    """
+    timeComplexity:  BigO(n^2)
+    spaceComplexity: BigO(n)
+    """
+    result = []
+
+    for idx, i in enumerate(numbers):
+        tmp = 1
+        for idj, j in enumerate(numbers):
+            if idx == idj:
+                continue
+
+            tmp *= j
+
+        result.append(tmp)
+
+    return result
 
 
-if __name__ == '__main__':
-    print(solution)
+def solution_two(numbers: list[int]) -> list[int]:
+    """
+    timeComplexity:  BigO(2n)
+    spaceComplexity: BigO(n)
+    """
+    total = 1
+
+    for n in numbers:
+        total *= n
+
+    result = []
+
+    for n in numbers:
+        result.append(total // n)
+
+    return result
+
+
+if __name__ == "__main__":
+    print(solution_one([1, 2, 3, 4, 5]))
+    print(solution_two([1, 2, 3, 4, 5]))
