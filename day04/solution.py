e3a2e8f38537167e7c5a2c3cc252eea8af0d448a
Henrique Leal <hm.leal@hotmail.com>
2021-09-30 14:09:11 +0100

    chore: Add solution for Problem 04

--- day04/solution.py
+++ day04/solution.py
@@ -1,6 +1,22 @@
-def solution():
-    pass
+def solution(numbers: list[int]) -> int:
+    """
+    timeComplexity:  WorstCase: BigO(2n) // BestCase: BigO(n)
+    spaceComplexity: BigO(n)
+    """
+    array_size = len(numbers)
+
+    found = [True] + [False] * (array_size - 1)
+
+    for n in numbers:
+        if 0 < n < array_size:
+            found[n] = True
+
+    for idx, n in enumerate(found):
+        if not n:
+            return idx
+
+    return array_size + 1
 
 
 if __name__ == "__main__":
-    print(solution())
+    print(solution([3, 4, -1, 1]))
